5c64c3274cce7907a13c9921b81dce361c82cb2e
JLuijten <135598395+JLuijten@users.noreply.github.com>
2025-11-24 21:21:08 +0100

    fixed path to files

--- Data/tornado_cleaner.py
+++ Data/tornado_cleaner.py
@@ -3,8 +3,8 @@
 import csv
 
 # data file to clean and cleaned file
-input_file = "us_tornado_dataset_1950_2021.csv"
-output_file = "clean_tornado_tx_1950_2021.csv"
+input_file = r"Data\us_tornado_dataset_1950_2021.csv"
+output_file = r"Data\clean_tornado_tx_1950_2021.csv"
 
 
 with open(input_file, newline="", encoding="utf-8") as infile, \
