c897f3a7472712028568716b8de8c3aabf0fd903
JLuijten <135598395+JLuijten@users.noreply.github.com>
2025-12-08 13:12:53 +0100

    made timeseries plot nicer

--- scripts/time-series.py
+++ scripts/time-series.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import os
 import matplotlib.pyplot as plt
 
-input_file = "../Data/final/final_data1.csv"
+input_file = r"Data\final\final_data1.csv"
 df = pd.read_csv(input_file)
 
 lat_list = []
@@ -21,14 +20,15 @@ fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.scatter(lat_min_1, lat_list, alpha = 0.1)
 ax1.set_xlabel("latitude (-1)")
 ax1.set_ylabel("latitude")
+ax1.set_ylim(25, 37)
+ax1.set_xlim(25, 37)
 
 ax2.scatter(lon_min_1, lon_list, alpha = 0.1)
 ax2.set_xlabel("longitude (-1)")
 ax2.set_ylabel("longitude")
+ax2.set_ylim(-107, -93)
+ax2.set_xlim(-107, -93)
 
-output_dir = '../plots'
-os.makedirs(output_dir, exist_ok=True)
-plt.savefig(os.path.join(output_dir, 'tornado_time_series.png'))
-print(f"Plot saved to {os.path.join(output_dir, 'tornado_time_series.png')}")
 
+plt.savefig(r"plots\tornado_time_series.png")
 plt.show()
