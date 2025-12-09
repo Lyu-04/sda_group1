224bb953bfea0eeb743cbeb3826cc5e5fb5e61e0
JLuijten <135598395+JLuijten@users.noreply.github.com>
2025-12-09 10:31:56 +0100

    added time series for whole storms, removed lineair fit

--- scripts/time-series.py
+++ scripts/time-series.py
@@ -17,22 +17,22 @@ for i in range(1, len(df["slat"])):
     lon_list.append(df["slon"][i])
     lon_min_1.append(df["slon"][i-1])
 
-#lineair fitting
+# data extraction for total storm data
+input_file = r"Data\processed\unique_storms.csv"
+storm_df = pd.read_csv(input_file)
+avg_lat_list = []
+avg_lat_min_1 = []
+avg_lon_list = []
+avg_lon_min_1 = []
+for i in range(1, len(storm_df)):
+    avg_lat_list.append(storm_df["avg_lat"][i])
+    avg_lat_min_1.append(storm_df["avg_lat"][i-1])
+    avg_lon_list.append(storm_df["avg_lon"][i])
+    avg_lon_min_1.append(storm_df["avg_lon"][i-1])
 
-a1, b1 = np.polyfit(lat_min_1, lat_list, 1)
-x_lat = np.arange(25, 38)
-lat_fit = a1 * x_lat + b1
-
-a2, b2 = np.polyfit(lon_min_1, lon_list, 1)
-x_lon = np.arange(-107, -92)
-lon_fit = a2 * x_lon + b2
-
-# calculate variance
-lat_residuals = []
-lon_residuals = []
 
 # plotting
-fig, (ax1, ax2) = plt.subplots(1, 2)
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 
 ax1.scatter(lat_min_1, lat_list, alpha = 0.1)
 #ax1.plot(x_lat, lat_fit, label= "lineair fit", color = "black")
@@ -48,6 +48,17 @@ ax2.set_ylabel("longitude")
 ax2.set_ylim(-107, -93)
 ax2.set_xlim(-107, -93)
 
+ax3.scatter(avg_lat_min_1, avg_lat_list, alpha = 0.1)
+ax3.set_xlabel("avg latitude (-1)")
+ax3.set_ylabel("avg latitude")
+ax3.set_ylim(25, 37)
+ax3.set_xlim(25, 37)
+
+ax4.scatter(avg_lon_min_1, avg_lon_list, alpha = 0.1)
+ax4.set_xlabel("avg longitude (-1)")
+ax4.set_ylabel("avg longitude")
+ax4.set_ylim(-107, -93)
+ax4.set_xlim(-107, -93)
 
 plt.savefig(r"plots\tornado_time_series.png")
 plt.show()
